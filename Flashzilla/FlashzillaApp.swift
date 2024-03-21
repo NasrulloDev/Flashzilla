69393a3a48a2a69a8b73e9d27a11e816e37190d1
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-21 23:30:22 +0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Насрулло Исмоилжонов on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
