6e70f9e721e0ab96c4046fa5fa3c12b1da08f8c2
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-23 11:37:58 +0500

    Card and CardView
    Designing a single card view

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Насрулло Исмоилжонов on 23/03/24.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
